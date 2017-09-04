eae1f1ea2d918d1eceb24993e267a35adbfb3690
Aleksander Smyk <aleksandersmyk@macbook-pro-aleksander.local>
2017-09-04 21:05:38 +0200

    Initial Commit

---
+++ cmdApp/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  cmdApp
+//
+//  Created by Aleksander Smyk on 22.08.2017.
+//  Copyright © 2017 Aleksander Smyk. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World! ...and Hello Aleksander")
+
